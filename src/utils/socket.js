cb50841d49bf910c6dcdf4194e7c6fc9bf5c45b3
poofy25 <117314241+poofy25@users.noreply.github.com>
2024-03-25 21:44:11 +0200

    handle server connection on orders page

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? undefined : 'https://restaurant-webapp-backend.onrender.com';
+const URL = 'https://restaurant-webapp-backend.onrender.com';
 
 export const socket = io(URL);
