adf8f6f1c0de9f5f964fa16445450dfe51b99d74
poofy25 <117314241+poofy25@users.noreply.github.com>
2024-03-31 09:40:15 +0300

    added menuItemInfo compoenent

--- src/components/MenuItem/MenuItem.jsx
+++ src/components/MenuItem/MenuItem.jsx
@@ -9,74 +9,87 @@ import infoSVG from '/public/svgs/info.svg'
 import { useCartContext }from '@/hooks/useCartContext'
 import { useState } from 'react'
 
+import MenuItemInfo from './MenuItemInfo/MenuItemInfo'
+
 export default function MenuItem ( {data} ) {
 
     const { dispatch } = useCartContext()
 
     const [addedToCart , setAddedToCart] = useState(false)
+    const [isOpenInfo , setIsOpenInfo] = useState(false)
 
     // Add item to cart context
     const handleCartBtn = () => {
         dispatch({type: 'ADD_ITEM', payload: data})
 
+        // Btn animation
         setAddedToCart(true)
         setTimeout(()=>{setAddedToCart(false)},2500)
+    }
+
+    const handleOpenInfo = () => {
+        setIsOpenInfo(true)
     }
     
 
     return (
-        <div className='
-            w-[clamp(100px,32vw,200px)] 
-            relative flex flex-col overflow-hidden items-center
-            p-2 box-border border rounded-lg gap-2 transition-all border-solid border-transparent
-            sm:w-[clamp(175px,20vw,250px)] sm:p-4
-          sm:hover:border-yellow-300
-        '>
-            {/* Item Image */}
-            <div className='w-full flex flex-col relative cursor-pointer z-[1] rounded-lg overflow-hidden'>
-                <Image src={data.imageUrl} width="350" height="500" alt='Item Image' loading='lazy'
-                className='aspect-square sm:aspect-[10/14] flex-1 w-full object-cover  z-[-1] '
-                />
-                <p className='sm:hidden absolute left-2 bottom-2 z-1'>{data.name}</p>
-                <div className='sm:hidden z-[-1]  absolute w-full h-full bg-gradient-to-b from-transparent to-[rgba(0,0,0,0.5)]'></div>
+        <>
+            <div className='
+                w-[clamp(100px,32vw,200px)] 
+                relative flex flex-col overflow-hidden items-center
+                p-2 box-border border rounded-lg gap-2 transition-all border-solid border-transparent
+                sm:w-[clamp(175px,20vw,250px)] sm:p-4
+            sm:hover:border-yellow-300
+            '>
+                {/* Item Image */}
+                <div onClick={handleOpenInfo}
+                className='w-full flex flex-col relative cursor-pointer z-[1] rounded-lg overflow-hidden'>
+                    <Image src={data.imageUrl} width="350" height="500" alt='Item Image' loading='lazy'
+                    className='aspect-square sm:aspect-[10/14] flex-1 w-full object-cover  z-[-1] '
+                    />
+                    <p className='sm:hidden absolute left-2 bottom-2 z-1 pr-2 box-border'>{data.name}</p>
+                    <div className='sm:hidden z-[-1]  absolute w-full h-full bg-gradient-to-b from-transparent to-[rgba(0,0,0,0.5)]'></div>
+                </div>
+
+                {/* Item name */}
+                <div className='hidden sm:flex w-full justify-between items-center p-2 box-border'>
+                    <h3 className='truncate'>{data.name}</h3>
+                    <button onClick={handleOpenInfo}
+                    className='bg-transparent invert border-0 cursor-pointer p-0 flex justify-center items-center'
+                    ><Image src={infoSVG} width="24" height="24" alt='Info Icon'/></button>
+                </div>
+                
+                {/* To cart button */}
+                <button className={`
+                    w-full flex justify-between bg-transparent p-2 cursor-pointer transition-all box-border text-white
+                    border border-yellow-300 border-solid rounded-lg
+                    hover:bg-yellow-300 hover:text-black
+                    ${styles.toCartBtn}
+                    ${addedToCart && 'bg-yellow-300 !text-black'} 
+                    `} 
+                    onClick={handleCartBtn}
+                    disabled={addedToCart}
+                >
+
+                    {!addedToCart ? 
+                        // Display message on add the cart
+                        <>
+                            <h4 className=''>{data.price} mdl</h4>
+                            <p className='hidden sm:flex ml-auto mr-2'>În coș</p>
+                            <Image src={toCartSVG} width="24" height="24" alt='Cart Icon'/>
+                        </> : 
+                        <>
+                            <p className='hidden sm:flex self-center'>Adaugat în coș!</p>
+                            <p className='flex sm:hidden self-center'>În coș!</p>
+                            <Image src={toCartSVG} width="24" height="24" alt='Cart Icon' className='!invert-0'/>
+                        </>
+                    } 
+
+                </button>
+                
             </div>
 
-            {/* Item name */}
-            <div className='hidden sm:flex w-full justify-between items-center p-2 box-border'>
-                <h3 className='truncate'>{data.name}</h3>
-                <button
-                className='bg-transparent invert border-0 cursor-pointer p-0 flex justify-center items-center'
-                ><Image src={infoSVG} width="24" height="24" alt='Info Icon'/></button>
-            </div>
-            
-            {/* To cart button */}
-            <button className={`
-                w-full flex justify-between bg-transparent p-2 cursor-pointer transition-all box-border text-white
-                border border-yellow-300 border-solid rounded-lg
-                hover:bg-yellow-300 hover:text-black
-                ${styles.toCartBtn}
-                ${addedToCart && 'bg-yellow-300 !text-black'} 
-                `} 
-                onClick={handleCartBtn}
-                disabled={addedToCart}
-            >
-
-                {!addedToCart ? 
-                    // Display message on add the cart
-                    <>
-                        <h4 className=''>{data.price} mdl</h4>
-                        <p className='hidden sm:flex ml-auto mr-2'>În coș</p>
-                        <Image src={toCartSVG} width="24" height="24" alt='Cart Icon'/>
-                    </> : 
-                    <>
-                        <p className='hidden sm:flex self-center'>Adaugat în coș!</p>
-                        <p className='flex sm:hidden self-center'>În coș!</p>
-                        <Image src={toCartSVG} width="24" height="24" alt='Cart Icon' className='!invert-0'/>
-                    </>
-                } 
-
-            </button>
-            
-        </div>
+            {isOpenInfo && <MenuItemInfo data={data} isOpenInfo={isOpenInfo} setIsOpenInfo={setIsOpenInfo}/>}
+        </>
     )
 }
---
+++ src/components/MenuItem/MenuItemInfo/MenuItemInfo.jsx
@@ -0,0 +1,93 @@
+'use client'
+
+import styles from './menuItemInfo.module.scss'
+
+import Image from "next/image"
+
+import toCartSVG from '/public/svgs/cart.svg'
+import infoSVG from '/public/svgs/info.svg'
+
+import { useCartContext }from '@/hooks/useCartContext'
+
+import { useState } from 'react'
+
+export default function MenuItemInfo ({data , isOpenInfo , setIsOpenInfo}) {
+    const { dispatch } = useCartContext()
+
+    const [addedToCart , setAddedToCart] = useState(false)
+
+    // Add item to cart context
+    const handleCartBtn = () => {
+        dispatch({type: 'ADD_ITEM', payload: data})
+
+        // Btn animation
+        setAddedToCart(true)
+        setTimeout(()=>{setAddedToCart(false)},2500)
+    }
+
+    const handleCloseInfo = () => {
+        setIsOpenInfo(false)
+    }
+    
+
+    return (
+        <div className='fixed top-0 left-0 w-full h-full z-20 bg-primary no-doc-scroll p-4 box-border flex flex-col items-center gap-4'>
+            {/* Item Image */}
+            <button onClick={handleCloseInfo}
+            className='w-full rounded-lg bg-red-600'
+            >Închide</button>
+
+            <div className='w-full flex flex-col relative z-[1] rounded-lg overflow-hidden'>
+                <Image src={data.imageUrl} width="350" height="500" alt='Item Image' loading='lazy'
+                className='aspect-square sm:aspect-[10/14] flex-1 w-full object-cover  z-[-1] '
+                />
+            </div>
+
+            {/* Item name */}
+            <div className='flex w-full justify-between items-center px-2 box-border'>
+                <h3 className='m-0'>{data.name}</h3>
+            </div>
+
+            {/* Item description */}
+            <div className='flex w-full justify-between items-center px-2 box-border'>
+                <p>{data.description}</p>
+            </div>
+
+            {/* Item price */}
+            <div className='flex w-full justify-between items-center px-2 box-border'>
+                <h2>{data.price} lei</h2>
+                <h4>{data.weight} g</h4>
+            </div>
+            
+            {/* To cart button */}
+            <button className={`
+                w-full flex justify-between items-center bg-transparent p-2 py-4 cursor-pointer transition-all box-border
+                border border-yellow-300 border-solid rounded-lg
+                bg-yellow-300 text-black
+                hover:bg-yellow-300
+                ${styles.toCartBtn}
+                ${addedToCart && '!bg-transparent !text-white'} 
+                `} 
+                onClick={handleCartBtn}
+                disabled={addedToCart}
+            >
+
+                {!addedToCart ? 
+                    // Display message on add the cart
+                    <>
+                        <div className='w-[24px]'></div>
+                        <h3 className='flex'>Adaugă în coș</h3>
+                        <Image src={toCartSVG} width="24" height="24" alt='Cart Icon'/>
+                    </> : 
+                    <>
+                        <div className='w-[24px]'></div>
+                        <h3 className='flex'>Adăugat în coș !</h3>
+                        <Image src={toCartSVG} width="24" height="24" alt='Cart Icon'/>
+                    </>
+                } 
+
+            </button>
+            
+        </div>
+    )
+}
