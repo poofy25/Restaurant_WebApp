8330871c47c9b13c0f8bb70cbeb8f4543a63f4d4
poofy25 <117314241+poofy25@users.noreply.github.com>
2024-04-13 15:54:42 +0300

    fixed an import

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,10 +1,9 @@
-import HeroImg from '/public/imgs/hero.jpg'
 import Image from 'next/image'
 
 export default function HeroSection () {
     return (
         <div className='w-full h-[calc(100vh-80px)]'>
-            <div className='relative w-full h-auto aspect-square'><Image src={HeroImg} className='object-cover' fill={true}/></div>
+            {/* <div className='relative w-full h-auto aspect-square'><Image src={HeroImg} className='object-cover' fill={true}/></div> */}
         </div>
     )
 }
